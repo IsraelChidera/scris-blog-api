9f15ed3743a30146b8b566699482717571625702
IsraelChidera <israelchidera54@gmail.com>
2026-03-16 12:41:40 +0100

    update

--- index.js
+++ index.js
@@ -36,11 +36,12 @@ app.use((err, _req, res, _next) => {
     res.status(500).json({ error: 'Internal server error' });
 });
 
-mongoose.connect(process.env.MONGO_URI)
+
+mongoose.connect(config.get('mongoURI'))
     .then(() => {
         console.log('Connected to MongoDB');
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
+        app.listen(config.get('port'), () => {
+            console.log(`Server running on port ${config.get('port')}`);
         });
     })
     .catch(err => {
